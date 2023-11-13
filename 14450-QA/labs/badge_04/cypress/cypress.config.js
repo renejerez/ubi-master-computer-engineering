715d6af01b01627dfcf8759128c1f1af79ad48f5
Rene Jerez <rene_jerez@hotmail.com>
2023-11-13 16:21:21 +0000

    final

--- 14450-QA/labs/badge_04/cypress/cypress.config.js
+++ 14450-QA/labs/badge_04/cypress/cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'n6t8mi',
   e2e: {
+    experimentalStudio: true,
+    //testIsolation:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
